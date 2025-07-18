a92441a37b51869a36aec44e51c7e15bf6b46a79
Naman Chauhan <namanrajputnn001@gmail.com>
2025-07-18 22:30:50 +0530

    Update Register.jsx

--- client/src/Pages/Register.jsx
+++ client/src/Pages/Register.jsx
@@ -26,7 +26,7 @@ export const Register = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/register", {
+      const response = await fetch("https://excel-analytics-project.onrender.com/api/auth/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
