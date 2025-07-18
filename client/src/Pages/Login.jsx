4f670ab1f5cadbf224bcdf3e45194f3868b574fb
Naman Chauhan <namanrajputnn001@gmail.com>
2025-07-18 22:31:29 +0530

    Update Login.jsx

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -24,7 +24,7 @@ export const Login = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/login", {
+      const response = await fetch("https://excel-analytics-project.onrender.com/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
